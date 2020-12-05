5cca5ce86d13323250c24e0082ab2dffdd78eed0
riggle5 <rkysssssss1867@gmail.com>
2020-12-05 17:21:38 +0900

    remove unecessary code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { useState, useRef, useCallback } from 'react'
-import { Canvas, useLoader, useFrame, useThree } from 'react-three-fiber'
+import { Canvas, useLoader, useFrame} from 'react-three-fiber'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import { Physics, usePlane, useBox, useSphere } from '@react-three/cannon'
 import lerp from './utils/lerp';
 
 const Plane = (props) => {
-  const [ref] = usePlane(() => ({ rotation: [-Math.PI / 2, 0, 0], ...props }))
+  const [ref] = usePlane(() => ({ rotation: [-Math.PI / 2, 0, 0], ...props }));
+
   return (
     <mesh ref={ref} receiveShadow>
       <planeBufferGeometry attach="geometry" args={[1200, 1200]} />
@@ -15,13 +16,14 @@ const Plane = (props) => {
 }
 
 const Sphere = (props) => {
-  const [ref, api] = useSphere(() => ({ mass: 1, position: [0, 5, 0], ...props }))
-  let values = useRef([0, 0])
-   useFrame((state) => {
-    values.current[0] = lerp(values.current[0], (state.mouse.x * Math.PI) / 5, 10)
-    values.current[1] = lerp(values.current[1], (state.mouse.x * Math.PI) / 5, 10)
-    api.position.set(state.mouse.x * 10, 0, state.mouse.y * -10)
-  })
+  const [ref, api] = useSphere(() => ({ mass: 1, position: [0, 5, 0], ...props }));
+  let values = useRef([0, 0]);
+  useFrame((state) => {
+    values.current[0] = lerp(values.current[0], (state.mouse.x * Math.PI) / 5, 10);
+    values.current[1] = lerp(values.current[1], (state.mouse.x * Math.PI) / 5, 10);
+    api.position.set(state.mouse.x * 10, 0, state.mouse.y * -10);
+  });
+
   return (
     <mesh ref={ref}>
       <sphereBufferGeometry attach="geometry" />
@@ -31,14 +33,15 @@ const Sphere = (props) => {
 }
 
 const Geometory = (props) => {
-  const [ref] = useBox(() => ({  mass: 1, args: [0.1, 3.1, 3.1], rotation: [0, 0, 0], ...props }))
-  const gltf = useLoader(GLTFLoader, props.emoji), [geometry, setGeometry] = useState();
-
+  const [ref] = useBox(() => ({  mass: 1, args: [0.1, 3.1, 3.1], rotation: [0, 0, 0], ...props }));
+  const gltf = useLoader(GLTFLoader, props.emoji);
+  const [geometry, setGeometry] = useState();
   // https://github.com/pmndrs/react-three-fiber/issues/245 GLTFの複数配置
   if (!geometry) {
 		const scene = gltf.scene.clone(true); 
     setGeometry(scene);
-	}
+  }
+
   return (
     <mesh receiveShadow castShadow ref={ref} >
      	<primitive castShadow object={geometry} dispose={null} args={[2, 2, 2]} />
@@ -47,16 +50,15 @@ const Geometory = (props) => {
 }
 
 const App = () => {
-  const mouse = useRef([0, 0])
-  const onMouseMove = useCallback(({ clientX: x, clientY: y }) => (mouse.current = [x - window.innerWidth / 2, y - window.innerHeight / 2]), [])
+  const mouse = useRef([0, 0]);
+  const onMouseMove = useCallback(({ clientX: x, clientY: y }) => (mouse.current = [x - window.innerWidth / 2, y - window.innerHeight / 2]), []);
 
 	return (
     <Canvas 
-    shadowMap sRGB gl={{ alpha: false }} camera={{ position: [0, 12, 10], rotation: [0, 0, 0], fov: 90 }}
+      shadowMap sRGB gl={{ alpha: false }} camera={{ position: [0, 12, 10], rotation: [0, 0, 0], fov: 90 }}
       style={{height: '100vh',width: '100vw'}}
       onMouseMove={onMouseMove}
     >
-      {/* <fog attach="fog" args={['black', 50, 200]} /> */}
       <color attach="background" args={['plum']} />
       <hemisphereLight intensity={0.35} />
       <spotLight position={[30, 0, 30]} angle={0.3} penumbra={1} intensity={2} castShadow shadow-mapSize-width={256} shadow-mapSize-height={256} />
